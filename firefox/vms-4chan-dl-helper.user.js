f17861a9fa1168af1e9eaae30d60376459e0402f
Aries <7oucem93@gmail.com>
2026-03-25 16:14:07 +0100

    Create vms-4chan-dl-helper.user.js
    placeholder file

---
+++ firefox/vms-4chan-dl-helper.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name          4chan image download helper script
+// @version       0.0.1.2026.03.24
+// @match         https://boards.4chan.org/*
+// @author        https://github.com/aries-db
+// @description   download images from 4chan threads
+// ==/UserScript==
